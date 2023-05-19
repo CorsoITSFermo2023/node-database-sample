bad5a8529a6566b723706c6a62bc4700b759fe5e
Brian <cobyb1211@gmail.com>
2023-05-19 17:36:14 +0200

    sdd

--- dao.js
+++ dao.js
@@ -36,3 +36,4 @@ module.exports = {
   listaRigheScontrino,
   listaScontrini
 }
+
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const { all } = require("./db");
+const { listaScontrini, listaRigheScontrino, inserisciTotale } = require("./dao")
+const { all, insert  } = require("./db");
 
 async function run() {
-  const rows = await all('SELECT * FROM scontrino');
+  const rows = await all('SELECT * FROM scontrini');
   console.log(rows);
+
 }
+
 
 run();
