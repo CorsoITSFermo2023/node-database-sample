4f22ca09dad8d1dda8bcd1af1ee7ce195daa7584
Crescenzi.Francesco <crescenzi.francesco@itssmartacademy.it>
2023-05-19 18:00:42 +0200

    lol

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
+const { id } = require("date-fns/locale");
 const { listaScontrini } = require("./dao");
+const { inserisciTotale } = require("./dao");
+const {  listaRigheScontrino } = require("./dao") ;
 
 async function run() {
   const rows = await listaScontrini();
+  for(let i = 0;i<rows.lenght; i++)  {
+    const scontrino = rows[i]
+    const promessaRighe = listaRigheScontrino(scontrino.id)
+    const righe = await promessaRighe;
+  }
   console.log(rows);
 }
 
+function calcolagiornata() {
+  somma = 0;
+  
+  
+
+     
+
+  
+  }
+
+
+
+}
 run();
