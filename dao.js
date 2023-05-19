2d4622a22e3e38e8c7a85e0469f466c75cec81b1
Fabrizio Marconi <fabrizio.marconi@jef.it>
2023-05-19 15:33:01 +0200

    minor fix comments

--- dao.js
+++ dao.js
@@ -2,7 +2,7 @@ const { all, insert } = require("./db")
 
 /**
  * 
- * @returns {Array<{id: number, datetime: string}>}
+ * @returns {Promise<Array<{id: number, datetime: string}>>}
  */
 async function listaScontrini() {
   return await all('SELECT * FROM scontrino');
@@ -11,7 +11,7 @@ async function listaScontrini() {
 /**
  * 
  * @param {number} id_scontrino 
- * @returns {Array<{id: number, id_scontrino: number, descrizione: string, price: number, qty: number}>}
+ * @returns {Promise<Array<{id: number, id_scontrino: number, descrizione: string, price: number, qty: number}>>}
  */
 async function listaRigheScontrino(id_scontrino) {
   return await all('SELECT * FROM riga_scontrino WHERE id_scontrino = ?', [id_scontrino]);
@@ -22,7 +22,8 @@ async function listaRigheScontrino(id_scontrino) {
  * @param {Date} data 
  * @param {number} totale 
  * @param {number} numero_scontrini 
- * @param {number} media 
+ * @param {number} media
+ * @returns {Promise<undefined>} 
  */
 async function inserisciTotale(data, totale, numero_scontrini, media) {
   return await insert(
