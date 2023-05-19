b61fa0b813045e5d295df83ea58579dc9bea0f0a
Fabrizio Marconi <fabrizio.marconi@jef.it>
2023-05-19 18:43:31 +0200

    demo

--- index2.js
+++ index2.js
@@ -5,35 +5,35 @@ const { listaRigheScontrino } = require("./dao")
 const { inserisciTotale } = require("./dao")
 const parse = require('date-fns/parse')
 
+
 async function run() {
   const rows = await listaScontrini()
   //console.log(parse(rows[0].datetime))
 
-  let date= []
+  const date = {
+
+  }
 
 // for(const row of rows)
   for(let i=0;i<rows.length;i++){
     const dataConvertita = format(fromUnixTime(rows[i].dataorario/1000),"yyyy-MM-dd");
-    if(!date.includes(dataConvertita)) {
-        date.push(dataConvertita)
+    if(!date[dataConvertita]) {
+        date[dataConvertita]=[]
     }
+    date[dataConvertita].push(rows[i])
   }
 
-  for(let data=0;data<date.length;data++){
+  
+  for(const data in date){
     let totale=0
     let qty_scontrini=0
-    
+    const rows = date[data];
     for(let i=0;i<rows.length;i++){
-      const dataConvertita = format(fromUnixTime(rows[i].dataorario/1000),"yyyy-MM-dd");
-
-      if(date[data]==dataConvertita){
-          qty_scontrini++
-          const scontrino= await listaRigheScontrino(rows[i].id)
-          for(let j=0;j<scontrino.length;j++) totale+= scontrino[j].price * scontrino[j].qty
-      }//if
+      qty_scontrini++
+      const scontrino= await listaRigheScontrino(rows[i].id)
+      for(let j=0;j<scontrino.length;j++) totale+= scontrino[j].price * scontrino[j].qty
     }//for
-    
-    const Totale = await inserisciTotale(date[data],totale,qty_scontrini,totale/qty_scontrini)
+    const Totale = await inserisciTotale(data,totale,qty_scontrini,totale/qty_scontrini)
     console.log(date[data])
   }//for 
 }//run
