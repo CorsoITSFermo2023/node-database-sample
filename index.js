3499e1f8731a6e1f742ee2986afece65881d26a3
miri <plakumirjan03@gmail.com>
2023-05-19 18:39:26 +0200

    comme

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const { listaScontrini } = require("./dao");
+const {listaRigheScontrino} = require("./dao");
 
 async function run() {
-  const rows = await listaScontrini();
+  const  rows = await listaScontrini();
   console.log(rows);
+  let totale;
+  let date=[];
+
+  for(let i=0;i<rows.length;i++){
+    console.log(rows[i].datetime)
+  }
 }
 
 run();
