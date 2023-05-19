b9bdd9fd398daa5d2ad6250b177c7d5b8535357d
Francesco Sciarroni <francescosciarroni0@gmail.com>
2023-05-19 17:36:46 +0200

    Primo tentativo

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const { listaScontrini } = require("./dao");
+const { listaScontrini, listaRigheScontrino, inserisciTotale } = require("./dao");
 
+let date= [];
 async function run() {
   const rows = await listaScontrini();
   console.log(rows);
+
+  for (let i=0; i<rows.length; i++){
+    if (!date.includes(rows[i].datetime)){
+      date.push(rows[i].datetime)
+    }
+  }
+
+  for (let i=0; i<date.length; i++){
+    let totale= 0;
+    let qty= 0;
+    data=i;
+    for (let i=0; i<rows.length; i++){
+      if(date[data]==rows[i].datetime){
+        qty++;
+        const scontrino= listaRigheScontrino(rows[i].id);
+        totale=+ scontrino.price*scontrino.qty;
+        
+      }
+    }
+    stampa= inserisciTotale(date[i], totale, qty, )
+  }
 }
 
 run();
